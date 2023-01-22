cb87eff58e40dbf1ef4ffa0a1332489decf0036c
Hanna-Kom <anna.join.kr@gmail.com>
2023-01-22 22:09:31 +0100

    Homework 2

--- src/Task21.java
+++ src/Task21.java
@@ -1,16 +1,4 @@
-//public class Task21 {
-  //  public static void main(String[] args) {
-   //     int a = 7;
-    //    int b = -5;
-     //   if(a>b){
-//            System.out.println("Max value" + " " + a);
-//            System.out.println("Min value" + " " + b);
-//        }else {
-//            System.out.println("Max value" + " " + b);
-//            System.out.println("Min value" + " " + a);
-//        }
-//
-//    }
+
 
 public class Task21 {
     public static void main(String[] args) {
